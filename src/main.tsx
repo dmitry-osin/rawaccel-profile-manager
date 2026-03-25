4b5e3cda70d82468f8ec9f464c56fc1cf64ad73e
Dmitry Osin <d@osin.pro>
2026-03-26 01:24:00 +0300

    [+] material colors, styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./styles/themes.css";
+
+document.documentElement.setAttribute("data-theme", "dark");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App/>
+        <App />
     </React.StrictMode>,
 );
